29ea48c447aeca5e2dabc8740299c8a1d0591e4f
mrimon <rmamun1585@gmail.com>
2025-04-30 00:44:14 +0600

    modify some code for responsive

--- src/Components/TailwindCommand/TailwindCommand.jsx
+++ src/Components/TailwindCommand/TailwindCommand.jsx
@@ -4,7 +4,7 @@ import { SiReactrouter } from 'react-icons/si';
 
 const TailwindCommand = () => {
     return (
-        <div className='pt-20  px-10 text-blue-400 bg-blue-100 rounded-3xl shadow-2xl mt-8 '>
+        <div className='pt-20  px-10  text-blue-400 bg-blue-100 rounded-3xl shadow-2xl mt-8 '>
             <div className='flex items-center justify-center gap-2'>
                 <RiTailwindCssFill size={40} />
                 <span className='text-5xl text- text-center font-semibold'>Tailwind CSS Commands to Install.</span>
@@ -18,7 +18,7 @@ const TailwindCommand = () => {
 
                 <div className='my-8 border rounded-2xl bg-gray-200 border-gray-400 p-2 w-full md:w-6/12 mx-auto'>
                     <p className='my-4 text-xl text-center'>vite.config.ts</p>
-                    <code className='border bg-gray-100 w-full  border-gray-400 shadow-sm rounded-2xl block p-3 text-xl  text-[#5c9de7]'>
+                    <code className='border bg-gray-100 w-full break-words  border-gray-400 shadow-sm rounded-2xl block p-3 text-xl  text-[#5c9de7]'>
                         <span className=''>import tailwindcss from '@tailwindcss/vite'</span> <br />
                         <pre><code>
                             {`export default defineConfig({
